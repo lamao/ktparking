845f8cbae2777c7f8bbd13fc3f9feb5d4611ffe9
vmischeryakov <vmischeryakov@gmail.com>
2018-02-01 11:58:31 +0200

    Make repo tests transactional

--- src/test/kotlin/org/invenit/ktparking/repository/ParkingRepositoryTest.kt
+++ src/test/kotlin/org/invenit/ktparking/repository/ParkingRepositoryTest.kt
@@ -9,6 +9,7 @@ import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.junit4.SpringRunner
+import org.springframework.transaction.annotation.Transactional
 
 /**
  * Created by vyacheslav.mischeryakov
@@ -16,6 +17,7 @@ import org.springframework.test.context.junit4.SpringRunner
  */
 @RunWith(SpringRunner::class)
 @SpringBootTest
+@Transactional
 class ParkingRepositoryTest {
     @Autowired
     lateinit var parkingRepository: ParkingRepository
